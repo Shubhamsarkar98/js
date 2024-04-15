767896e25203d84db2a8f9e73a805e5673c24bad
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-04-15 10:16:31 +0530

    foreach

--- index.js
+++ index.js
@@ -2,3 +2,39 @@
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 // console.log(fruits.length)
+
+
+//JavaScript Array Foreach vs Map
+
+//foreach not return any thing
+// const a=fruits.forEach((data)=>{
+//     console.log(data)
+// })
+
+// const items = ["item1", "item2", "item3"];
+// const copyItems = [];
+
+// // before
+// for (let i = 0; i < items.length; i++) {
+//   copyItems.push(items[i]);
+// }
+
+// // after
+// items.forEach((item) => {
+//   copyItems.push(item);
+// });
+
+// console.log(copyItems)
+//foreach if you print define new value you not print the value like that
+// const items = ["item1", "item2", "item3"];
+
+// const item=items.forEach((data)=>{
+//     console.log(data)
+// })
+
+// console.log(item)
+
+//the item value is undfied
+
+//----------------------------------Map----------------------------------------------//
+
