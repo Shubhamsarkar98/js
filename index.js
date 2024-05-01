10c86ab5116a7f9d54ce3941f387cb79382fede9
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-05-01 14:33:37 +0530

    add object mathod

--- index.js
+++ index.js
@@ -387,3 +387,14 @@
 // let person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
 
 // console.log(person.lastName)
+//----------------///--------------object soma mathod---------------------------------------------------//
+function Person(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+  }
+  
+  const myFather = new Person("John", "Doe", 50, "blue");
+  const myMother = new Person("Sally", "Rally", 48, "green");
+  console.log(myFather.age)
