ffdf542286e429675069303d6e8890f76935be8e
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-05-02 11:58:08 +0530

    add some unwented code

--- index.js
+++ index.js
@@ -388,13 +388,13 @@
 
 // console.log(person.lastName)
 //----------------///--------------object soma mathod---------------------------------------------------//
-function Person(first, last, age, eyecolor) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
-  }
+// function Person(first, last, age, eyecolor) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eyecolor;
+//   }
   
-  const myFather = new Person("John", "Doe", 50, "blue");
-  const myMother = new Person("Sally", "Rally", 48, "green");
-  console.log(myFather.age)
+//   const myFather = new Person("John", "Doe", 50, "blue");
+//   const myMother = new Person("Sally", "Rally", 48, "green");
+//   console.log(myFather.age)
